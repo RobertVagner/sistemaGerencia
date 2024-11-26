226589aff3dedf36809783778a5b29156722cc44
Mairon <maironteodoro@gmail.com>
2024-11-26 01:04:41 -0300

    Busca permissao Endpoint funcionando

--- sistemaGerencia/Models/PermissaoModel.cs
+++ sistemaGerencia/Models/PermissaoModel.cs
@@ -1,10 +1,13 @@
-﻿namespace sistemaGerencia.Models
+﻿using System.Text.Json.Serialization;
+
+namespace sistemaGerencia.Models
 {
     public class PermissaoModel
     {
         public int IdPermissao { get; set; }
         public string? NomePermissão { get; set; }
         public string? DescricaoPermissao { get; set; }
+        [JsonIgnore]
         public ICollection<GrupoModel>? grupos { get; set; }
     }
 }
--- sistemaGerencia/obj/Debug/net6.0/sistemaGerencia.AssemblyInfo.cs
+++ sistemaGerencia/obj/Debug/net6.0/sistemaGerencia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistemaGerencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca3a4051662162b8c83e11b28f29539cf9b180e")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistemaGerencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistemaGerencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
